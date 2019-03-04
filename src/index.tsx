80715dab0425f45ec4b999a7018c3ae5d0346756
m430 <360475097@qq.com>
2019-03-04 09:37:43 +0800

    change all code to typescript

---
+++ src/index.tsx
@@ -0,0 +1,71 @@
+export { default as ActiveChart } from './components/ActiveChart';
+
+export { default as ArticleListContent } from './components/ArticleListContent';
+
+export { default as Authorized } from './components/Authorized';
+
+export { default as AvatarList } from './components/AvatarList';
+
+export { default as Charts } from './components/Charts';
+
+export { default as CountDown } from './components/CountDown';
+
+export { default as CusInputNumber } from './components/CusInputNumber';
+
+export { default as DecriptionList } from './components/DescriptionList';
+
+export { default as EditableItem } from './components/EditableItem';
+
+export { default as EditableLinkGroup } from './components/EditableLinkGroup';
+
+export { default as Ellipsis } from './components/Ellipsis';
+
+export { default as Exception } from './components/Exception';
+
+export { default as FooterToolbar } from './components/FooterToolbar';
+
+export { default as GlobalFooter } from './components/GlobalFooter';
+
+export { default as GlobalHeader } from './components/GlobalHeader';
+
+export { default as HeaderSearch } from './components/HeaderSearch';
+
+export { default as Headline } from './components/Headline';
+
+export { default as Login } from './components/Login';
+
+export { default as MenuView } from './components/MenuView';
+
+export { default as NoticeIcon } from './components/NoticeIcon';
+
+export { default as NumberInfo } from './components/NumberInfo';
+
+export { default as PageHeader } from './components/PageHeader';
+
+export { default as PageHeaderWrapper } from './components/PageHeaderWrapper';
+
+export { default as PageLoading } from './components/PageLoading';
+
+export { default as Range } from './components/Range';
+
+export { default as Result } from './components/Result';
+
+export { default as ScrollSelect } from './components/ScrollSelect';
+
+export { default as SelectLang } from './components/SelectLang';
+
+export { default as SettingDrawer } from './components/SettingDrawer';
+
+export { default as SiderMenu } from './components/SiderMenu';
+
+export { default as StandardFormRow } from './components/StandardFormRow';
+
+export { default as StandardTable } from './components/StandardTable';
+
+export { default as TagSelect } from './components/TagSelect';
+
+export { default as TopNavHeader } from './components/TopNavHeader';
+
+export { default as Transfer } from './components/Transfer';
+
+export { default as Trend } from './components/Trend';
